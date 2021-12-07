9e4473f0c8da16ef5d27d209b5f1a1eb2cdb6978
Hamimi Zouaoui <zouaoui.hm@gmail.com>
2021-12-07 22:11:59 +0100

    added helmet

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
+const helmet = require('helmet');
 const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config();
@@ -22,11 +23,15 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(helmet());
+
 app.use(bodyParser.json());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
+
+app.disable('x-powered-by');
 
 module.exports = app;
